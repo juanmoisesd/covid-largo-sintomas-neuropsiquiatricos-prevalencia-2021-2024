3d9bc94df62852e353e115344f8f1337dd3478cc
Juan Moises de la Serna <jmsernatuya@gmail.com>
2026-03-25 09:05:13 +0100

    Add/update covid_largo_sintomas_neur/__init__.py

--- covid_largo_sintomas_neur/__init__.py
+++ covid_largo_sintomas_neur/__init__.py
@@ -2,7 +2,7 @@
 DOI: https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/QUWC1F | GitHub: https://github.com/juanmoisesd/covid-largo-sintomas-neuropsiquiatricos-prevalencia-2021-2024"""
 __version__="1.0.0"
 __author__="de la Serna, Juan Moisés"
-import pandas as pd, io
+import pandas as pd,io
 try:
     import requests
 except ImportError:
@@ -13,9 +13,9 @@ def load_data(filename=None):
     rid="https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/QUWC1F".split(".")[-1]
     meta=requests.get(f"https://zenodo.org/api/records/{rid}",timeout=30).json()
     csvs=[f for f in meta.get("files",[]) if f["key"].endswith(".csv")]
-    if not csvs: raise ValueError("No CSV files found")
+    if not csvs:raise ValueError("No CSV found")
     f=next((x for x in csvs if filename and x["key"]==filename),csvs[0])
     return pd.read_csv(io.StringIO(requests.get(f["links"]["self"],timeout=60).text))
 
-def cite(): return f'de la Serna, Juan Moisés (2025). Prevalencia global de síntomas neurológicos y psiquiátricos del COVID Largo (nie. Zenodo. https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/QUWC1F'
-def info(): print(f"Dataset: Prevalencia global de síntomas neurológicos y psiquiátricos del COVID Largo (nie\nDOI: https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/QUWC1F\nGitHub: https://github.com/juanmoisesd/covid-largo-sintomas-neuropsiquiatricos-prevalencia-2021-2024")
+def cite():return f'de la Serna, Juan Moisés (2025). Prevalencia global de síntomas neurológicos y psiquiátricos del COVID Largo (nie. Zenodo. https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/QUWC1F'
+def info():print(f"Dataset: Prevalencia global de síntomas neurológicos y psiquiátricos del COVID Largo (nie\nDOI: https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/QUWC1F\nGitHub: https://github.com/juanmoisesd/covid-largo-sintomas-neuropsiquiatricos-prevalencia-2021-2024")
